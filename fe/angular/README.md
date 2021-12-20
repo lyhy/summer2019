e6ef102112c9babaa38d3e25fb61302a64ac2911
junwang-zingbox <junwang@zingbox.com>
2021-12-20 06:17:17 -0800

    vim udemy added

--- fe/angular/README.md
+++ fe/angular/README.md
@@ -2,3 +2,6 @@
 -npx degit repo#branchname
 -npx degit repo#v1.2.3 //release tag
 -npx degit repo#aaaaaaa //commit bash
+
+# compare git branch
+https://github.com/angular-university/ngrx-course/compare/1-start...2-entity-finished
--- fe/angular/auxiliary-route/angular_university/angular-router-course/src/app/app-routing.module.ts
+++ fe/angular/auxiliary-route/angular_university/angular-router-course/src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules, UrlSerializer} from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'courses',
+    
 
+
+  }
 ];
 
 @NgModule({
   imports: [
-
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule],
   providers: [
