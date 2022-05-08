dd9374fed7fe7a5cd97d43dd40daa8145547c2e1
junwang-zingbox <junwang@zingbox.com>
2022-05-08 09:06:20 -0700

    clean-webpack-plugin added

--- webpack/udemy_webpack_from_beginner_to_advanced/webpack.config.js
+++ webpack/udemy_webpack_from_beginner_to_advanced/webpack.config.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const M = require('minimatch');
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -51,6 +50,12 @@ module.exports = {
 		new TerserPlugin(),
 		new MiniCssExtractPlugin({
 			filename: 'styles.[contenthash].css'
-	})
+	}),
+	new CleanWebpackPlugin({
+		cleanOnceBeforeBuildPatterns: [
+				'**/*',
+				path.join(process.cwd(), 'build/**/*')
+		]
+})
 ]
 }
