14f841021d9b227d23b019d9f7565b7d148e5720
junwang-zingbox <junwang@zingbox.com>
2022-05-08 10:57:32 -0700

    lodash split chunk

--- webpack/udemy_webpack_from_beginner_to_advanced/src/hello-world.js
+++ webpack/udemy_webpack_from_beginner_to_advanced/src/hello-world.js
@@ -1,7 +1,8 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
 import Heading from './components/heading/heading.js';
+import _ from 'lodash'
 const heading = new Heading();
-heading.render();
+heading.render('hello world');
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
